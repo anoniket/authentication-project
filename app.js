a76a1d072bc376302dc1e258adef5487fb782969
Aniket khandelwal <anonymous.eww@gmail.com>
2020-06-29 14:36:21 +0530

    added level 5, installed new packages and initialised them

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 //jshint esversion:6
+//the commented coded means older level of authentication!
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-const bcrypt = require('bcrypt');
+// const encrypt = require("mongoose-encryption");
+// const bcrypt = require('bcrypt');
 
 // const md5 = require("md5");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 
 const app = express();
 //salting round number dont increase it too much, as time increases exponentially.
-const saltRounds = 10;
+// const saltRounds = 10;
 
 app.use(express.static("public"));
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(session({
+  secret:"Dhoom Machale",
+  resave:false,
+   saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,useUnifiedTopology: true});
 const db = mongoose.connection;
@@ -24,16 +37,25 @@ db.once('open', function() {
   console.log("Einates have captured mongoose");
 });
 
+mongoose.set("useCreateIndex",true);
+
 const userSchema = new mongoose.Schema({
   email:String,
   password:String
 });
 
 
+userSchema.plugin(passportLocalMongoose);
 // userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields: ["password"]});
 
 
 const User = new mongoose.model("User",userSchema);
+
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 
 app.get("/",function(req,res){
@@ -51,35 +73,35 @@ app.get("/register",function(req,res){
 
 app.post("/register",function(req,res){
 
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-      // Store hash in your password DB.
-      const newUser = new User({
-        email:req.body.username,
-        password:hash
-      });
-      newUser.save(function(err){
-        if(!err){
-          res.render("secrets");
-        } else {console.log(err);}
-      });
-  });
+  // bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+  //     // Store hash in your password DB.
+  //     const newUser = new User({
+  //       email:req.body.username,
+  //       password:hash
+  //     });
+  //     newUser.save(function(err){
+  //       if(!err){
+  //         res.render("secrets");
+  //       } else {console.log(err);}
+  //     });
+  // });
 })
 
 
 app.post("/login",function(req,res){
-  User.findOne({email:req.body.username},function(err,userFound){
-    if(!err){
-      if(userFound){
-        bcrypt.compare(req.body.password, userFound.password, function(err, result) {
-    // result == true
-    if(!err){
-      if(result){res.render("secrets");}else{res.send("Incorrect password");}
-    }
-          });
-      }else{res.send("User not found");}
-    }else{res.send(err);}
-  })
-})
+  // User.findOne({email:req.body.username},function(err,userFound){
+  //   if(!err){
+  //     if(userFound){
+  //       bcrypt.compare(req.body.password, userFound.password, function(err, result) {
+  //   // result == true
+  //   if(!err){
+  //     if(result){res.render("secrets");}else{res.send("Incorrect password");}
+  //   }
+  //         });
+  //     }else{res.send("User not found");}
+  //   }else{res.send(err);}
+  // })
+});
 
 
 
